8ecb1a51f4776a59c2733f356987c13cffbc0f3b
Marcel Andrioli <marcelandrioli@gmail.com>
2021-01-22 15:11:31 -0300

    Projeto criado

---
+++ backend/src/main/java/br/com/marcelandrioli/apiescola/ApiEscolaApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelandrioli.apiescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEscolaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/marcelandrioli/apiescola/ApiEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelandrioli.apiescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
